2d0897132b96feed299a3943da981f4bce27f22a
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-01 12:17:55 -0300

    Feature: Add Reactotron config and to console

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import './src/config/reactotron.config';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text>Yeey Reactotron is now available</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
