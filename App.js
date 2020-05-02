029c3ed5632fef03ce75e72db28286a5fd172425
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-02 02:54:03 -0300

    Feature: Add firebase auth test with hard code

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
+import FirebaseService from './src/services/FirebaseService';
 
 import './src/config/reactotron.config';
 
 export default function App() {
+
+
+  useEffect(() => {
+
+    function auth() {
+      const user = FirebaseService.auth('brun@email.com', 'bruno@123');
+      if (user.hasError) {
+        console.tron.log('error => ', user)
+      }
+    };
+
+    auth();
+    
+  }, [])
+
   return (
     <View>
       <Text>Yeey Reactotron is now available</Text>
