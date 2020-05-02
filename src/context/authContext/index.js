3a1eebbf050a82d145262592cd37a07c71b225d0
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-02 12:07:46 -0300

    Feature: Add Auth context and Auth Hooks (add .env.example)

--- src/context/authContext/index.js
+++ src/context/authContext/index.js
@@ -7,17 +7,50 @@ import {
     LOGIN_FAILURE,
 } from '../constants';
 
+const AuthStateContext = React.createContext();
+const AuthDispatchContext = React.createContext();
+
+AuthStateContext.displayName = 'AuthStateContext';
+AuthDispatchContext.displayName = 'AuthDispatchContext';
 
 
-
+// TODO: Implement function to each action
 function authReducer(state, action) {
     switch (action.type) {
         case LOGIN_SUCCESS:
             break;
         case LOGIN_REQUEST:
-            FirebaseService.auth()
             break;
         case LOGIN_FAILURE:
             break;
     }
 }
+
+function AuthProvider({children}) {
+    const [state, dispatch] = React.useReducer(authReducer, {email: '', password: ''});
+    return (
+        <AuthStateContext.Provider value={state}>
+            <AuthDispatchContext.Provider value={dispatch}>
+                {children}
+            </AuthDispatchContext.Provider>
+        </AuthStateContext.Provider>
+    )
+}
+
+function useAuthState() {
+    const context = React.useContext(AuthStateContext);
+    if (context === undefined) {
+        throw new Error('useAuthContext must be used within a AuthProvider')
+    }
+    return context;
+}
+
+function useAuthDispatch() {
+    const context = React.useContext(AuthDispatchContext);
+    if (context === undefined) {
+        throw new Error('useAuthContext must be used within a AuthProvider');
+    }
+    return context;
+}
+
+export {AuthProvider, useAuthState, useAuthDispatch}
