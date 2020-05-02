bd4d75b3970ec82d05cca4d95eb09aa5344d24b1
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-02 16:17:40 -0300

    Feature: Add Theme Provider
    - Remove Auth tests

--- App.js
+++ App.js
@@ -1,28 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Text, View } from 'react-native';
-import FirebaseService from './src/services/FirebaseService';
-
+import Theme from './src/theme';
 import './src/config/reactotron.config';
 
 export default function App() {
-
-
-  useEffect(() => {
-
-    function auth() {
-      const user = FirebaseService.auth('brun@email.com', 'bruno@123');
-      if (user.hasError) {
-        console.tron.log('error => ', user)
-      }
-    };
-
-    auth();
-    
-  }, [])
-
   return (
-    <View>
-      <Text>Yeey Reactotron is now available</Text>
-    </View>
+    <Theme>
+      <View>
+        <Text>Yeey Reactotron is now available</Text>
+      </View>
+    </Theme>
   );
 }
