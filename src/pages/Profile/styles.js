c4f4be567c6595d7c0028c72094d62de67df1ecc
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-03 18:02:49 -0300

    Feature: Add Profile Form

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,9 +1,8 @@
+
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
 
-`;
+
+
+
