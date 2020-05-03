cac567c628af671ce807ce1a270c781237d2957e
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-03 18:04:43 -0300

    Feature: Add components to component index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
+import AddImageButton from './AddImageButton';
 import Button from './Button';
+import Container from './Container';
 import Input from './Input';
+import Label from './Label';
 
 export {
+    AddImageButton,
     Button,
+    Container,
     Input,
+    Label,
 };
