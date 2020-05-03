5fb9a206e482f75e354fc559b94f2115741810e3
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-03 17:07:17 -0300

    Feature: Add configuration for header and back button on routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,30 +1,46 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import arrowLeft from '../../assets/img/arrow-left-header.png'
+
 import SignUp from '../pages/SignUp';
 import Profile from '../pages/Profile';
 import Login from '../pages/Login';
 
 const AuthStack = createStackNavigator();
+
+const theme = {
+    color: {
+        brandPrimary:'#16B4A1',
+        white: '#fff',
+    }
+};
+
 const AuthRoutes = () => {
 
-
+    console.tron.log({arrowLeft})
     return (
         <AuthStack.Navigator
             screenOptions={{
-                headerShown: false, 
-                cardStyle: {
-                    backgroundColor: 'transparent',
-                }
+                headerTintColor: theme.color.white,
+                headerStyle: {
+                    backgroundColor: theme.color.brandPrimary,
+                },
+                headerBackTitle: "Voltar"
             }}
         >
             <AuthStack.Screen
                 name="SignUp"
                 component={SignUp}
+                options={{headerShown: false}}
             />
             <AuthStack.Screen
                 name="Profile"
                 component={Profile}
+                options={{
+                    title: 'Bem vindo ao Raise',
+                    headerLeft: null,
+                }}
             />
             <AuthStack.Screen
                 name="Login"
