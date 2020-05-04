cad7dbfbd94eafbe012c70ae7e71a7d21412788d
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-03 22:19:48 -0300

    Feature: Add Profile page layout and logic

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Text, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 
 import { 
   Container,
@@ -9,25 +9,73 @@ import {
   Label,
 } from '../../components';
 
-export default function Profile() {
+import {Title, Paragraph} from './styles';
+
+export default function Profile({navigation}) {
+
+  const nameRef = useRef();
+  const ageRef = useRef();
+  const cityRef = useRef();
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [city, setCity] = useState('');
+
+  const verifyFields = () => {
+    return !(name !== '' && age !== '' && city !== '')
+  }
+
   return (
-    <Container>
-        <Text>Precisamos de algumas informações para o seu perfil</Text>
+    <ScrollView>
+      <Container>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+          style={{flex: 1}}
+        >
+        
+          <Title>Precisamos de algumas informações para o seu perfil</Title>
 
-        <AddImageButton onPress={() => console.tron.log('banana')}/>
-        <Text>Escolha uma foto para o seu perfil</Text>
-        <Text>(opcional)</Text>
+          <AddImageButton onPress={() => console.tron.log('banana')}/>
+          <Paragraph>Escolha uma foto para o seu perfil (opcional)</Paragraph>
 
-        <Label>Informe seu nome:</Label>
-        <Input darkBorder />
+          <Label>Informe seu nome:</Label>
+          <Input 
+            darkBorder 
+            onChangeText={setName}
+            value={name}
+            ref={nameRef}
+            returnKeyType="next"
+            onSubmitEditing={() => ageRef.current.focus()}
+          />
 
-        <Label>Informe a sua idade:</Label>
-        <Input darkBorder />
+          <Label >Informe a sua idade:</Label>
+          <Input 
+            darkBorder 
+            style={{width: 80}}
+            onChangeText={setAge}
+            value={age}
+            ref={ageRef}
+            returnKeyType="next"
+            onSubmitEditing={() => cityRef.current.focus()}
+            enablesReturnKeyAutomatically={true}
+          />
 
-        <Label>Informe a sua cidade:</Label>
-        <Input darkBorder />
+          <Label>Informe a sua cidade:</Label>
+          <Input 
+            darkBorder
+            onChangeText={setCity}
+            value={city}
+            ref={cityRef}
+            returnKeyType="send"
+            onSubmitEditing={() => console.tron.log('tabatinga')}
+          />
 
-        <Button disabled>Próximo</Button>
-    </Container>
+          <Button 
+            disabled={verifyFields()}
+            onPress={() => {/*navigation.navigate()*/} }
+          >Próximo</Button>
+        </KeyboardAvoidingView>
+        </Container>
+      </ScrollView>
   );
 }
