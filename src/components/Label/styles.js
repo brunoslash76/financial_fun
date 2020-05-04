a268f9ed030d1b28c7e7ff65669fa5e20bb33327
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-03 22:18:21 -0300

    Fix: Solve object destructurin to dot notation

--- src/components/Label/styles.js
+++ src/components/Label/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.Text`
-    font-size: ${({theme: { font: { size } }}) => size.title};
-    color: ${({theme: { colors: { grey }}}) => grey};
+    font-size: ${props => props.theme.font.size.title};
+    color: ${props => props.theme.colors.grey};
     margin-bottom: 8px;
     align-self: flex-start;
 `;
