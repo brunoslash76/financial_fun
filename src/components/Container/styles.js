ed6fb7923c4e09266d3243743b600c421e4222ea
Bruno Tavares de Moraes Russo <btmrusso@gmail.com>
2020-05-04 02:29:42 -0300

    Refactor: Remove unused styles

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -2,9 +2,7 @@ import styled from 'styled-components/native';
 
 export const MainContainer = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+
 `;
 
 export const ContentContainer = styled.View`
