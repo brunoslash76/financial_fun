348a405cfce94770fde18ae4c471e908dfe1619b
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-06-15 11:06:17 -0300

    Feature: Add Placeholder for Dependent Profile without picture

---
+++ src/components/Placeholder/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  border-radius: 50;
+  background-color: #999;
+  height: 72px;
+  width: 72px;
+  justify-content: flex-start;
+    margin-right: 16px;
+`;
