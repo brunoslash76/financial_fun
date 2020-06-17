fac81d33df5d51a89dc97eb308363ef79e466a37
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-06-17 17:58:30 -0300

    Feature: Add fix to border radius

--- src/components/Placeholder/styles.js
+++ src/components/Placeholder/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  border-radius: 50;
+  border-radius: 36px;
   background-color: #999;
   height: 72px;
   width: 72px;
