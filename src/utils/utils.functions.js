41c4292beb798ee04f2588106bc045658e812cc8
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-07-02 17:19:22 -0300

    Feature: Add Email and Password Validation Utils

--- src/utils/utils.functions.js
+++ src/utils/utils.functions.js
@@ -1,9 +1,19 @@
 export function isEmpty(obj) {
-    for(var prop in obj) {
-      if(obj.hasOwnProperty(prop)) {
-        return false;
-      }
-    }
-  
-    return JSON.stringify(obj) === JSON.stringify({});
-  }
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop)) {
+			return false;
+		}
+	}
+
+	return JSON.stringify(obj) === JSON.stringify({});
+}
+
+export function checkIfIsEmail(string) {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(string);
+}
+
+export function verifyPassword(string) {
+  const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+  return regex.test(string);
+}
